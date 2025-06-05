64959c00581498530156470c8a39c3f7af47151d
Leo <ismaelaspuacr.5@gmail.com>
2025-06-05 10:34:34 -0600

    Actulizacion2

--- assets/js/js.js
+++ assets/js/js.js
@@ -27,13 +27,79 @@ setSlider();
 
 // set diameter
 const setDiameter = () => {
-    let slider = document.querySelector('.slider');
+    // Se actualiza el selector para el slider principal
+    let slider = document.querySelector('.slider'); // Anteriormente: '.sliders'
     let widthSlider = slider.offsetWidth;
     let heightSlider = slider.offsetHeight;
     let diameter = Math.sqrt(Math.pow(widthSlider, 2) + Math.pow(heightSlider, 2));
-    document.documentElement.style.setProperty('--diameter', diameter+'px');
+    document.documentElement.style.setProperty('--diameter', diameter + 'px');
 }
 setDiameter();
 window.addEventListener('resize', () => {
     setDiameter();
 })
+
+
+/*Content */
+document.addEventListener('DOMContentLoaded', () => {
+    const sliderItems = document.querySelectorAll('.items');
+    const carousel = document.querySelector('.banner .sliders');
+
+    sliderItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const cardId = item.getAttribute('data-card');
+            const cardDetail = document.querySelector(`.card[data-card="${cardId}"]`);
+            if (cardDetail) {
+                cardDetail.classList.add('active');
+            }
+            if (carousel) {
+                carousel.style.animationPlayState = 'paused';
+            }
+        });
+    });
+
+    const closeBtns = document.querySelectorAll('.close-card');
+    closeBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const cardDetail = btn.closest('.card');
+            if (cardDetail) {
+                cardDetail.classList.remove('active');
+            }
+            if (carousel) {
+                carousel.style.animationPlayState = 'running';
+            }
+        });
+    });
+});
+
+
+/*Fases*/
+ $(".step").click( function() {
+	$(this).addClass("active").prevAll().addClass("active");
+	$(this).nextAll().removeClass("active");
+});
+
+$(".step01").click( function() {
+	$("#line-progress").css("width", "3%");
+	$(".discovery").addClass("active").siblings().removeClass("active");
+});
+
+$(".step02").click( function() {
+	$("#line-progress").css("width", "25%");
+	$(".strategy").addClass("active").siblings().removeClass("active");
+});
+
+$(".step03").click( function() {
+	$("#line-progress").css("width", "50%");
+	$(".creative").addClass("active").siblings().removeClass("active");
+});
+
+$(".step04").click( function() {
+	$("#line-progress").css("width", "75%");
+	$(".production").addClass("active").siblings().removeClass("active");
+});
+
+$(".step05").click( function() {
+	$("#line-progress").css("width", "100%");
+	$(".analysis").addClass("active").siblings().removeClass("active");
+});
